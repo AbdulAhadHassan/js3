f902ac0bd541ab3bb00fd17746286ee8d506c211
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-03-07 00:01:49 -0800

    js3

---
+++ main.js
@@ -0,0 +1,163 @@
+// chapter 9-11
+
+// Q1
+// var city = prompt('Enter ur country')
+// if(city='Karachi'){
+//        document.write(
+//         '<p>', 'WELCOME TO THE CITY OF LIGHT' , '</p>'
+//        )
+// }
+
+
+// Q2
+// var gender = prompt('Enter ur gender')
+// if(gender == 'male'){
+//     document.write( 'Good morning sir' )
+// }
+// else if(gender=='female'){
+//     document.write('<p>', "Good morning ma'am" , '</p>')
+// }
+// else {
+//     document.write('<p>', "you gay?" , '</p>')
+// }
+
+
+// Q3
+// var color = prompt('Enter color')
+// if(color == 'red'){
+//     document.write( 'Must Stop ' )
+// }
+// else if(color=='green'){
+//     document.write('<p>', "Move now " , '</p>')
+// }
+// else if(color=='yellow'){
+//     document.write('<p>', "ready to move" , '</p>')
+// }
+// else {
+//     document.write('<p>', "this color is not in the input" , '</p>')
+// }
+ 
+
+
+// Q4
+// var fuel = prompt('Enter color')
+// if(fuel <= '0.25'){
+//     document.write( 'Please refill the fuel in your car' )
+// }
+// else{
+//     document.write('<p>', " Your goood to go " , '</p>')
+// }
+
+// Q5
+
+// a       true 
+// var a = 4; 
+// if (++a === 5){ 
+// alert("given condition for variable a is true"); 
+// }
+
+
+
+
+// b     false
+// var b = 82; 
+// if (b++ === 83){ 
+// alert("given condition for variable b is true"); } 
+
+
+
+
+// c     
+
+// false
+
+// var c = 12; 
+// if (c++ === 13){ 
+// alert("condition 1 is true"); 
+// } 
+
+// true
+
+// if (c === 13){                         
+// alert("condition 2 is true"); 
+// } 
+
+// false
+
+// if (++c < 14){ 
+// alert("condition 3 is true"); 
+// } 
+
+// true
+
+// if(c === 14){ 
+// alert("condition 4 is true"); 
+// } 
+
+
+
+// d    true
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost){ alert("The cost equals"); 
+// } 
+
+
+
+// e   true
+// if (true){ 
+//     alert("True"); 
+// } 
+// if (false){ 
+//     alert("False"); 
+// }
+    
+
+
+// f   true
+// if("car" < "cat"){ 
+//     alert("car is smaller than cat"); 
+// }
+
+
+
+
+// Q6
+// var grade = +prompt("enter ur marks");
+// var percentage = ((grade / 300)* 100)+ '%';
+
+// if( 240 <= grade ){
+//     document.write('<p>', " Total marks : 300"  , '</p>')
+//     document.write('<p>', " Marks obtain :  "  , grade , '</p>')
+//     document.write('<p>', " Percentage : " , percentage , '</p>')
+//     document.write('<p>', " Grade : A-one"  , '</p>')
+//     document.write('<p>', " Remark : Keep it up" , '</p>')
+// }
+// else if( 210 <= grade ){
+//     document.write('<p>', " Total marks : 300"  , '</p>')
+//     document.write('<p>', " Marks obtain :  "  , grade , '</p>')
+//     document.write('<p>', " Percentage : " , percentage , '</p>')
+//     document.write('<p>', " Grade : A"  , '</p>')
+//     document.write('<p>', " Remark : good" , '</p>')
+// }
+// else if( 180 <= grade ){
+//     document.write('<p>', " Total marks : 300"  , '</p>')
+//     document.write('<p>', " Marks obtain :  "  , grade , '</p>')
+//     document.write('<p>', " Percentage : " , percentage , '</p>')
+//     document.write('<p>', " Grade : B"  , '</p>')
+//     document.write('<p>', " Remark : need impovment" , '</p>')
+// }
+// else if( 180 > grade ){
+//     document.write('<p>', " Total marks : 300"  , '</p>')
+//     document.write('<p>', " Marks obtain :  "  , grade , '</p>')
+//     document.write('<p>', " Percentage : " , percentage , '</p>')
+//     document.write('<p>', " Grade : F "  , '</p>')
+//     document.write('<p>', " Remark : Better luck next time" , '</p>')
+// }
+// else{
+//     document.write('<p>', " Enter number pls"  , '</p>')
+
+// }
+
+
